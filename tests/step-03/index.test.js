895bed53debdf1ce45198190f50ae5c84f2d60b7
kabir-afk <kabirsingh12b@gmail.com>
2024-04-05 13:42:39 +0530

    Test 3: added error query format

--- tests/step-03/index.test.js
+++ tests/step-03/index.test.js
@@ -4,15 +4,20 @@ const parseQuery = require('../../src/queryParser');
 test('Read CSV File', async () => {
     const data = await readCSV('./sample.csv');
     expect(data.length).toBeGreaterThan(0);
-    expect(data.length).toBe(3);
-    expect(data[0].name).toBe('John');
-    expect(data[0].age).toBe('30'); //ignore the string type here, we will fix this later
+    expect(data.length).toBe(5);
+    expect(data[0].age == data[1].age);
 });
 
 test('Parse SQL Query', () => {
-    const query = 'SELECT id, name FROM sample';
-    const parsed = parseQuery(query);
-    expect(parsed).toEqual({
+    const sampleQuery = 'SELECT id, name FROM sample';
+    const errorQuery = 'SELECTT id, name FROM sample';
+    const parsed1 = parseQuery(sampleQuery);
+    expect(parsed1).toEqual({
+        fields: ['id', 'name'],
+        table: 'sample'
+    });
+    const parsed2 = parseQuery(errorQuery);
+    expect(parsed2).toEqual({
         fields: ['id', 'name'],
         table: 'sample'
     });
